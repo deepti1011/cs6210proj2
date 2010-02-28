ef0f30108878d2f1525e21110b9ab681dccc0fdd
bryanjh <bryanjh@8ce9cb14-200d-11df-980b-c1ecf858a722>
2010-02-28 21:05:11 +0000

    shm w/ binary data

--- trunk/test.c
+++ trunk/test.c
@@ -3,22 +3,27 @@
 #include <string.h>
 #include <stdio.h>
 
+struct point {
+  int x;
+  int y;
+};
+
 int main() {
   int fd = shm_open("testmem", O_RDWR | O_CREAT, 777);
-  char request[] = "test";
-  size_t size = sizeof(request);
-  write(fd, request, size);
+  struct point pt;
+  pt.x = 10;
+  pt.y = 21;
+  size_t size = sizeof(struct point);
+  write(fd, &pt, size);
 
-  void* received;
-  received = (char*)mmap(0, size, PROT_EXEC | PROT_READ | PROT_WRITE, 
+  struct point * pt2;
+  pt2 = (struct point *)mmap(0, size, PROT_EXEC | PROT_READ | PROT_WRITE, 
        MAP_SHARED, fd, 0);
 
-  if(received != MAP_FAILED) {
-    bzero(request, size);
-    snprintf(request, size, "%s", (char*)received);
-    printf("%s\n", request);
+  if(pt2 != MAP_FAILED) {
+    printf("%d %d\n", pt2->x, pt2->y);
   }
 
-  munmap(received, size);
+  munmap(pt2, size);
   shm_unlink("testmem");
 }
