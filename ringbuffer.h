658354902cbf076fa83819298b65fce97b78ccde
bryanjh <bryanjh@8ce9cb14-200d-11df-980b-c1ecf858a722>
2010-03-01 21:12:01 +0000

    deadlock

--- ringbuffer.h
+++ ringbuffer.h
@@ -6,29 +6,25 @@
 struct request {
   int x;
   int p;
-  struct response* result;
-};
-
-struct response {
-  int result;
   pthread_cond_t response_ready;
 };
 
 struct ring_buffer {
   struct request request_buffer[MAXSIZE];
-  int request_end;
-  int request_start;
+  unsigned int request_writes;
+  unsigned int request_reads;
   pthread_mutex_t request_mutex;
   pthread_cond_t nonempty;
 
-  struct response response_buffer[MAXSIZE];
-  int response_end;
-  int response_start;
+  int response_buffer[MAXSIZE];
+  unsigned int response_writes;
+  unsigned int response_reads;
   pthread_mutex_t response_mutex;
 };
 
 void init_ring_buffer(struct ring_buffer* rbuff);
 void write_request(struct ring_buffer* rbuff, int x, int p, 
 		   pthread_cond_t* response_ready);
-
+void read_request(struct ring_buffer* rbuff);
+void process_requests(struct ring_buffer* rbuff);
 
