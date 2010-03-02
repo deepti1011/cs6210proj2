b5250832ebfb344c3c286e47354899bf2f2dde43
bryanjh <bryanjh@8ce9cb14-200d-11df-980b-c1ecf858a722>
2010-03-02 01:26:31 +0000

    responses work

--- client.c
+++ client.c
@@ -11,27 +11,26 @@ int main() {
   shm_rbuff = (struct ring_buffer*)mmap(0, sizeof(struct ring_buffer),
 					PROT_EXEC | PROT_READ | PROT_WRITE,
 					MAP_SHARED, fd, 0);
-
+  close(fd);
   if(shm_rbuff == MAP_FAILED) {
     perror("Unable to map shared memory.");
-    close(fd);
     shm_unlink(DATA);
     return 1;
   }
-  close(fd);
   
   int x = 10;
   int p = 7;
-  pthread_cond_t* response_ready;
+    
+  int response = write_request(shm_rbuff, x, p);
   
-  pthread_mutex_lock(&shm_rbuff->response_mutex);
-  write_request(shm_rbuff, x, p, response_ready);
-  printf("request written\n");
-
-  pthread_cond_wait(response_ready, &shm_rbuff->response_mutex);
-  printf("condition satisfied\n");
-  pthread_mutex_unlock(&shm_rbuff->response_mutex);
-  printf("response ready\n");
+  pthread_mutex_lock(&shm_rbuff->data_mutex);
+  pthread_cond_wait(&shm_rbuff->response_ready[response], &shm_rbuff->data_mutex);
+  
+  int result;
+  result = shm_rbuff->response_buffer[shm_rbuff->response_reads % MAXSIZE];
+  printf("Request (%d, %d) = %d\n", x, p, result);
+  shm_rbuff->response_reads++;
+  pthread_mutex_unlock(&shm_rbuff->data_mutex);
 
   munmap(shm_rbuff, sizeof(struct ring_buffer));
   shm_unlink(DATA);
