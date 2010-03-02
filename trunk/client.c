358b06b62d7c7cb928e8ad401a15a20188ec0a6b
bryanjh <bryanjh@8ce9cb14-200d-11df-980b-c1ecf858a722>
2010-03-02 03:17:48 +0000

    100% working

--- trunk/client.c
+++ trunk/client.c
@@ -3,8 +3,48 @@
 #include <stdio.h>
 #include <sys/stat.h>
 #include <fcntl.h>
+#include <unistd.h>
+#include <limits.h>
+#include <math.h>
+#include <sys/time.h>
+
+#define PRIME 97
+#define INTERVAL 10
+#define NUM_THREADS 128
+
+void* make_requests(void* param) {
+  struct ring_buffer* rbuff = (struct ring_buffer*)param; 
+
+  int i;
+ loop:
+  for  (i = 0; pow(2.0, i) < INT_MAX; i++) {
+    struct timeval start, end;
+    gettimeofday(&start, NULL);
+    int response = write_request(rbuff, i, PRIME);
+
+    if(response == -1)
+      continue;
+
+    pthread_mutex_lock(&rbuff->data_mutex);
+    pthread_cond_wait(&rbuff->response_ready[response],
+		      &rbuff->data_mutex);
+
+    int result;
+    result = rbuff->response_buffer[rbuff->response_reads % MAXSIZE];
+    
+    rbuff->response_reads++;
+    pthread_mutex_unlock(&rbuff->data_mutex);
+    gettimeofday(&end, NULL);
+    unsigned int time = end.tv_usec - start.tv_usec;
+    printf("Request (%d, %d) = %d. Time elapsed(us) %u\n", i, PRIME, 
+	   result, time);
+
+    usleep(10);
+  }
+}
 
 int main() {
+  pthread_t threads[NUM_THREADS];
   int fd = shm_open(DATA, O_RDWR | O_CREAT, S_IRWXU | S_IRWXO);
 
   struct ring_buffer* shm_rbuff;
@@ -18,19 +58,17 @@ int main() {
     return 1;
   }
   
-  int x = 10;
-  int p = 7;
-    
-  int response = write_request(shm_rbuff, x, p);
-  
-  pthread_mutex_lock(&shm_rbuff->data_mutex);
-  pthread_cond_wait(&shm_rbuff->response_ready[response], &shm_rbuff->data_mutex);
-  
-  int result;
-  result = shm_rbuff->response_buffer[shm_rbuff->response_reads % MAXSIZE];
-  printf("Request (%d, %d) = %d\n", x, p, result);
-  shm_rbuff->response_reads++;
-  pthread_mutex_unlock(&shm_rbuff->data_mutex);
+  int i;
+  for(i = 0; i < NUM_THREADS; i++) {
+    if((pthread_create(&threads[i], NULL, make_requests, 
+		       (void*)shm_rbuff)) != 0) {
+      perror("Unable to create thread");
+      break;
+    }
+  }
+
+  for(i = 0; i < NUM_THREADS; i++) 
+    pthread_join(threads[i], NULL);
 
   munmap(shm_rbuff, sizeof(struct ring_buffer));
   shm_unlink(DATA);
