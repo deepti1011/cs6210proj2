a5902fa4c703ece420a1d793c0864e12012dbbb7
bryanjh <bryanjh@8ce9cb14-200d-11df-980b-c1ecf858a722>
2010-04-23 05:34:08 +0000

    unmap and destroy

--- project4/rvm.c
+++ project4/rvm.c
@@ -8,17 +8,24 @@
 
 #include "rvm.h"
 
+#define MAXSEGS 8
+
+segment mapped[MAXSEGS];
+int num_mapped;
+
 rvm_t rvm_init(const char *directory) {
   int dirlen;
   rvm_t store;
   char buffer[1024];
 
+  num_mapped = 0;
+  
   mkdir(directory, S_IRWXU);
 
   dirlen = strlen(directory);
 
   store.dir = (char*)malloc((dirlen + 2) * sizeof(char));
-  
+
   strcpy(store.dir, directory);
 
   if(store.dir[dirlen - 1] != '/') {
@@ -38,6 +45,11 @@ rvm_t rvm_init(const char *directory) {
 void* rvm_map(rvm_t rvm, const char *segname, int size_to_create) {
   int dirlen, seglen, fd, remains, i;
   void * result;
+
+  if(num_mapped == MAXSEGS) {
+    fprintf(stderr, "Maximum number of segments reached.\n");
+    return NULL;
+  }
 
   dirlen = strlen(rvm.dir);
   seglen = strlen(segname);
@@ -66,20 +78,77 @@ void* rvm_map(rvm_t rvm, const char *segname, int size_to_create) {
   char padding[remains];
   memset(padding, '\0', remains);
 
+  i = 0;
   while((i += write(fd, padding, remains - i)) < remains);
 
-  result = mmap(NULL, size_to_create, PROT_READ | PROT_WRITE, MAP_SHARED, fd, 0);
-  close(fd);
+  result = mmap(NULL, size_to_create, 
+		PROT_READ | PROT_WRITE, MAP_SHARED, fd, 0);
+  
+  if(remains == size_to_create) {
+    mapped[num_mapped].data = result;
+    mapped[num_mapped].size = size_to_create;
+    num_mapped++;
+  } else {
+    for(i = 0; i < num_mapped; i++) {
+      if(mapped[i].data == result) {
+	mapped[i].size = size_to_create;
+	break;
+      } else if(i == num_mapped - 1) {
+	mapped[num_mapped].data = result;
+	mapped[num_mapped].size = size_to_create;
+	num_mapped++;
+	break;
+      }
+    }
+  }
 
+  close(fd);
   return result;
+}
+
+void rvm_unmap(rvm_t rvm, void *segbase) {
+  int i;
+  for(i = 0; i < num_mapped; i++) {
+    if(segbase == mapped[i].data) {
+      
+      if(munmap(segbase, mapped[i].size) != 0) {
+	perror("Unable to unmap memory.");
+	return;
+      } else { break; }
+    } else if(i == num_mapped - 1) {
+      fprintf(stderr, "No record of memory to unmap.\n");
+      return;
+    }
+  }
+
+  for(;i < num_mapped - 1; i++) {
+    mapped[i].data = mapped[i + 1].data;
+    mapped[i].size = mapped[i + 1].size;
+  }
+  num_mapped--;
+}
+
+void rvm_destroy(rvm_t rvm, const char *segname) {
+  int name_len, dir_len;
+  
+  name_len = strlen(segname);
+  dir_len = strlen(rvm.dir);
+
+  char buffer[dir_len + name_len + 1];
+
+  strcpy(buffer, rvm.dir);
+  strcat(buffer, segname);
+  
+  remove(buffer);
 }
 
 int main() {
   rvm_t store;
   void* data;
+  int size;
   
   store = rvm_init("store");
-  rvm_map(store, "one", 1024);
+  data = rvm_map(store, "one", 1024);
 
   free(store.dir);
 }
